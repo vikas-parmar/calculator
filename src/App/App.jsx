eb7cdb959b0bb7abb56ba58598337aa9daa59d20
Vikas Panwar <vkparmar003@gmail.com>
2023-02-19 19:47:34 +0530

    added onClick function to get value.

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -3,31 +3,38 @@ import './App.css'
 
 function App() {
   const [result, setResult] = useState("");
+
+  function handleClick(e){
+    setResult(result.concat(e.target.name));
+  };
+
   return (
     <div className="container">
       <form>
         <input type="text" value={result} />
       </form>
+      
       <div className='keypad'>
-      <button>Clear</button>
+      <button>AC</button>
       <button>C</button>
-      <button>&divide;</button>
-      <button>7</button>
-      <button>8</button>
-      <button>9</button>
-      <button>&times;</button>
-      <button>4</button>
-      <button>5</button>
-      <button>6</button>
-      <button>&ndash;</button>
-      <button>1</button>
-      <button>2</button>
-      <button>3</button>
-      <button>+</button>
-      <button>0</button>
-      <button>.</button>
-      <button>=</button>
+      <button name='/' onClick={handleClick}>&divide;</button>
+      <button name='7' onClick={handleClick}>7</button>
+      <button name='8' onClick={handleClick}>8</button>
+      <button name='9' onClick={handleClick}>9</button>
+      <button name='*' onClick={handleClick}>&times;</button>
+      <button name='4' onClick={handleClick}>4</button>
+      <button name='5' onClick={handleClick}>5</button>
+      <button name='6' onClick={handleClick}>6</button>
+      <button name='-' onClick={handleClick}>&ndash;</button>
+      <button name='1' onClick={handleClick}>1</button>
+      <button name='2' onClick={handleClick}>2</button>
+      <button name='3' onClick={handleClick}>3</button>
+      <button name='+' onClick={handleClick}>+</button>
+      <button name='0' onClick={handleClick}>0</button>
+      <button name='.' onClick={handleClick}>.</button>
+      <button onClick={handleClick}>=</button>
       </div>
+
     </div>
   )
 }
