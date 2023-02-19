95991ece7e8cc0da1df65cacd52e1f0601d81ba0
Vikas Panwar <vkparmar003@gmail.com>
2023-02-19 19:30:11 +0530

    added keypad

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -8,7 +8,26 @@ function App() {
       <form>
         <input type="text" value={result} />
       </form>
-    
+      <div className='keypad'>
+      <button>Clear</button>
+      <button>C</button>
+      <button>&divide;</button>
+      <button>7</button>
+      <button>8</button>
+      <button>9</button>
+      <button>&times;</button>
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button>&ndash;</button>
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>+</button>
+      <button>0</button>
+      <button>.</button>
+      <button>=</button>
+      </div>
     </div>
   )
 }
