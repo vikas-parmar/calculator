15a4724880e201fc2c309088dd81138f7ae07ee3
Vikas Panwar <vkparmar003@gmail.com>
2023-02-19 17:21:49 +0530

    added input tag  to show the result.

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -2,10 +2,13 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-
+  const [result, setResult] = useState("");
   return (
     <div className="container">
-      
+      <form>
+        <input type="text" value={result} />
+      </form>
+    
     </div>
   )
 }
