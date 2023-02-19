e84608eb0a2068e17e25138a84f0af45952e4f95
Vikas Panwar <vkparmar003@gmail.com>
2023-02-19 20:45:24 +0530

    added clear & backspace function

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -8,6 +8,13 @@ function App() {
     setResult(result.concat(e.target.name));
   };
 
+  const clear= ()=>{
+    setResult("");
+  }
+  const backspace= ()=>{
+    setResult(result.slice(0,  - 1)); // or result.length -1
+  }
+
   return (
     <div className="container">
       <form>
@@ -15,24 +22,24 @@ function App() {
       </form>
       
       <div className='keypad'>
-      <button>AC</button>
-      <button>C</button>
-      <button name='/' onClick={handleClick}>&divide;</button>
+      <button onClick={clear} id='clear' className='highlight'>AC</button>
+      <button onClick={backspace} id='backspace'className='highlight'>C</button>
+      <button name='/' onClick={handleClick} className='highlight'>&divide;</button>
       <button name='7' onClick={handleClick}>7</button>
       <button name='8' onClick={handleClick}>8</button>
       <button name='9' onClick={handleClick}>9</button>
-      <button name='*' onClick={handleClick}>&times;</button>
+      <button name='*' onClick={handleClick} className='highlight'>&times;</button>
       <button name='4' onClick={handleClick}>4</button>
       <button name='5' onClick={handleClick}>5</button>
       <button name='6' onClick={handleClick}>6</button>
-      <button name='-' onClick={handleClick}>&ndash;</button>
+      <button name='-' onClick={handleClick} className='highlight'>&ndash;</button>
       <button name='1' onClick={handleClick}>1</button>
       <button name='2' onClick={handleClick}>2</button>
       <button name='3' onClick={handleClick}>3</button>
-      <button name='+' onClick={handleClick}>+</button>
+      <button name='+' onClick={handleClick} className='highlight'>+</button>
       <button name='0' onClick={handleClick}>0</button>
       <button name='.' onClick={handleClick}>.</button>
-      <button onClick={handleClick}>=</button>
+      <button onClick={handleClick} id='result' className='highlight'>=</button>
       </div>
 
     </div>
